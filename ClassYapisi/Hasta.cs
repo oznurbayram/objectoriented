f2eabde763e29a2add671b171de3a457bcbc11f0
oznurbayram <oz.nur.bayram@hotmail.com>
2018-12-22 16:46:14 +0300

    test

---
+++ ClassYapisi/Hasta.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassYapisi
+{
+   public class Hasta
+    {
+        public string ad;
+        public string soyad;
+        private string _TCNO;
+        public Muayene[] muayene;
+
+        //Encapsulation işlemi = dışarıdan erişimi yönetebilmektir
+        public string TCNO
+        {
+            get{
+                return _TCNO; }
+            set
+            {
+                if (value.Length == 11)
+                {
+                    if (!value.StartsWith("0"))
+                    {
+                        _TCNO = value;
+                        return;
+                    }
+                    Console.WriteLine("tc hatali kardes");
+                }
+
+            }
+        }
+    }
+}
---
+++ ClassYapisi/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassYapisi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hasta hasta = new Hasta();
+            hasta.ad = "a";
+            hasta.soyad = "b";
+            hasta.TCNO = "11111111111"; 
+
+            Console.WriteLine(hasta.TCNO);
+        }
+    }
+}
